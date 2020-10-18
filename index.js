31a39775cc76d325aab6d25ce24ce8f77d651e39
rishabhjoshi-rj <rishabh.joshi@impactqa.com>
2020-10-18 13:55:56 +0530

    Third commit

--- index.js
+++ index.js
@@ -22,9 +22,11 @@ class Book {
   
       const row = document.createElement('tr');         //create a tr 
   
-      row.innerHTML = `                            //enter data in tr 
+
+      //enter data in tr 
+      row.innerHTML = `                            
         <td>${book.title}</td>
-        <td>${book.author}</td>
+        <td>${book.author}</td>            
         <td>${book.isbn}</td>
         <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
       `;
